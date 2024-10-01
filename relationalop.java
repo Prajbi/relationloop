cf8b6fa62de0173027eb9d714f26637d663c9063
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-10-01 13:43:06 +0530

    Add files via upload

---
+++ relationalop.java
@@ -0,0 +1,12 @@
+public class relationalop {
+
+    public static void main(String[] args) {
+        int p = 15, q = 40;
+        System.out.println(p == q);// false
+        System.out.println(p != q);// true
+        System.out.println(p > q);// false
+        System.out.println(p < q);// true
+        System.out.println(p <= q);// false
+        System.out.println(p >= q);// false
+    }
+}
